877fc9e285c510540fcf9691b13cc437cb47eeb9
PerIvan <1and130699@gmail.com>
2019-04-07 23:33:24 +0300

    new task2-5.sh

--- task2-5.sh
+++ task2-5.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 #5. Найти в файловой системе все файлы, размером больше 1Кб и меньше 1 Мб. В файл записать имена таких файлов, 
 #расположенных в каталоге /etc.
-touch f1.txt
-touch f2.txt
 touch f3.txt
-echo "более 1кб"
-find ~/Desktop -size +1k -print
-find ~/Desktop -size +1k >> f1.txt
-echo "До  1Mб"
-find ~/Desktop -size -1024k -print 
-find ~/Desktop -size -1024k -print >> f2.txt
-diff -sr f1.txt f2.txt >> f3.txt
-rm f1.txt
-rm f2.txt
+echo "более 1кб, но менее 1Мб"
+find ~/Desktop -size +1k -size -1024k > f3.txt
+find ~/Desktop -size +1k -size -1024k
